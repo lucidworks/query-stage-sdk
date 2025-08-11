b5ea65193c52ba63a4d876bb0c214f9ad8ad820d
Diego Gutierrez <diego.guiterrez@lucidworks.com>
2025-08-11 18:37:59 -0500

    update build

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -1,3 +1,3 @@
-@Library("one-platform-jenkins") _
+@Library("one-platform-jenkins@FUS-7095-take3") _
 
-buildFusionPublicJavaLibraryK8s("query-stage-sdk", "11")
+buildFusionPublicJavaLibraryK8s("query-stage-sdk", "11", ['releaseMajorVersion': true])
